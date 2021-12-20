ef83a3c5ea42fb240dd3b02f8e15dbcf3823ca5d
costar011 <dnaer456@gmail.com>
2021-12-20 19:58:20 +0900

    btn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
+import BButton from "./BButton";
+import styled from "styled-components";
+import { Button } from "antd";
+
+const Wrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: ${(props) => (props.direction === "row" ? "row" : "column")};
+`;
+
+const Btn = styled.button`
+  width: 200px;
+  height: 25px;
+  margin: 10px;
+
+  outline: none;
+  border: ${(props) => (props.isBorder ? "1px solid #999" : "none")};
+  border-radius: 5px;
+
+  transition: 0.5s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: skyblue;
+    color: #999;
+  }
+`;
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <h1>
+        <BButton />
+      </h1>
+    );
   }
 }
 
---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class BButton extends React.Component {
+  render() {
+    return <h1>Bbutton</h1>;
+  }
+}
+
+export default BButton;
